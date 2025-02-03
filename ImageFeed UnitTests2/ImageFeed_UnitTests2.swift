4736c4622f06ef7411829b0db95fdf7676647aae
Islam <itagirov2003@gmail.com>
2025-02-03 18:27:51 +0300

    работа с файлами

---
+++ ImageFeed UnitTests2/ImageFeed_UnitTests2.swift
@@ -0,0 +1,16 @@
+//
+//  ImageFeed_UnitTests2.swift
+//  ImageFeed UnitTests2
+//
+//  Created by Islam Tagirov on 03.02.2025.
+//
+
+import Testing
+
+struct ImageFeed_UnitTests2 {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ ImageFeedUiTests2/ImageFeedUiTests2.swift
@@ -0,0 +1,16 @@
+//
+//  ImageFeedUiTests2.swift
+//  ImageFeedUiTests2
+//
+//  Created by Islam Tagirov on 03.02.2025.
+//
+
+import Testing
+
+struct ImageFeedUiTests2 {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
